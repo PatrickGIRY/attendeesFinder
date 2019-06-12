cce8a25fd6c4630439a189944fe220d45741517e
Patrick GIRY <patrickgiry@gmail.com>
2019-06-12 17:17:32 +0200

    add haskell starting point

---
+++ haskell/attendeesFinderSpec.hs
@@ -0,0 +1,21 @@
+import Test.Hspec
+import Data.List (isInfixOf)
+
+data Attendee = Attendee {
+     firstName :: String
+} deriving (Show, Eq)
+
+main = hspec $ do
+    let attendees = [Attendee { firstName = "Marc" }, 
+                     Attendee { firstName = "Christelle" },
+                     Attendee { firstName = "Christophe" }]
+
+    describe "The attendees finder should return" $ do
+        it "an empty result when no attendee first name matches the query string" $ do
+            findByInfixOfFirstName "Paul" attendees `shouldBe` []
+
+        it "one result when only one attendee first name matches the query string" $ do
+            findByInfixOfFirstName "Marc" attendees `shouldBe` [Attendee {firstName = "Marc"}]
+
+        it "all the results when many attendees first names match the query string" $ do
+            findByInfixOfFirstName "hri" attendees `shouldBe` [Attendee {firstName = "Christelle"}, Attendee {firstName = "Christophe"}]
